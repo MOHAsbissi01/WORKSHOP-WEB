4d1aeb8bbd03fa3a3f372b7f422bf1b595f4caf8
MOHAsbissi01 <116494947+MOHAsbissi01@users.noreply.github.com>
2024-02-15 07:51:50 +0100

    Scrollmotion with images & background
    Enabling interactive image movements and scroll-based motion effects.

---
+++ FT-END/main.js
@@ -0,0 +1,21 @@
+let stars = document.getElementById("stars");
+let moon = document.getElementById("moon");
+let mountains3 = document.getElementById("mountains3");
+let mountains4 = document.getElementById("mountains4");
+let river  = document.getElementById("river");
+let boat = document.getElementById("boat");
+let mountains7 = document.getElementById("mountains7");
+
+window.onscroll = function(){
+    let value = scrollY ;
+    console.log(value) ;
+    stars.style.left = value + 'px' ;
+    moon.style.top = value + 'px' ;
+    boat.style.top = value*0.5  + 'px' ;
+    boat.style.left = value*1  + 'px' ;
+    mountains3.style.top = value*0.5 + 'px' ;
+    mountains4.style.top = value*0.5 + 'px' ;
+    river.style.top = value*0.5 + 'px' ;
+    mountains7.style.top = value*0.5 + 'px' ;
+
+}  
